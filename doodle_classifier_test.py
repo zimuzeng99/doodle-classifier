04a2474db8dd7edc916b07f7579f13ad1b43835e
Zimu Zeng <zimu.zeng@gmail.com>
2019-01-18 23:37:57 +0000

    Achieved 91% accuracy by using 4-layer MLP

--- doodle_classifier_test.py
+++ doodle_classifier_test.py
@@ -1,55 +1,100 @@
 import neural_network
 import random
+import math
 import numpy as np
+from mnist import MNIST
+
+nn = neural_network.NeuralNetwork([784, 400, 400, 3], 0.2)
 
 car_data = np.load("car.npy")
 airplane_data = np.load("airplane.npy")
 aircraft_carrier_data = np.load("aircraft_carrier.npy")
 
-nn = neural_network.NeuralNetwork(784, 64, 3)
-
 def normalize(x):
-    return x / 255.0
+    return x / 255
 
-normalize_v = np.vectorize(normalize)
+v_normalize = np.vectorize(normalize)
 
-def index_max(output):
-    index = 0
-    max = 0
+training_size = 25000
 
-    if output[0][0] > max:
-        max = output[0][0]
-        index = 0
+# Normalizes the training set to improve learning effectiveness
+for i in range(0, training_size):
+    car_data[i] = normalize(car_data[i])
+    airplane_data[i] = normalize(airplane_data[i])
+    aircraft_carrier_data[i] = normalize(aircraft_carrier_data[i])
 
-    if output[1][0] > max:
-        max = output[1][0]
-        index = 1
+num_iterations = 0
+while num_iterations < 15:
+    for i in range(0, training_size):
+        nn.train(car_data[i].tolist(), [1, 0, 0])
+        nn.train(airplane_data[i].tolist(), [0, 1, 0])
+        nn.train(aircraft_carrier_data[i].tolist(), [0, 0, 1])
+    num_iterations += 1
 
-    if output[2][0] > max:
-        max = output[2][0]
-        index = 2
+num_correct = 0
 
-    return index
+for i in range(0, training_size):
+    # The output will be a vector of probabilities. We use the largest value
+    # in this vector to determine the digit that the NN recognized.
+    output = nn.compute(car_data[i])
+    doodle_class = output.index(max(output))
 
+    # If the NN predicted the correct digit
+    if doodle_class == 0:
+        num_correct += 1
 
-for i in range(0, 25000):
-    nn.train(normalize_v(car_data[i]).tolist(), [1.0, 0.0, 0.0])
-    nn.train(normalize_v(airplane_data[i]).tolist(), [0.0, 1.0, 0.0])
-    nn.train(normalize_v(aircraft_carrier_data[i]).tolist(), [0.0, 0.0, 1.0])
-    print(i)
+    output = nn.compute(airplane_data[i])
+    doodle_class = output.index(max(output))
 
-numCorrect = 0
-for i in range(30000, 32000):
-    output = index_max(nn.feed_forward(normalize_v(car_data[i]).tolist()))
-    if output == 0:
-        numCorrect += 1
+    # If the NN predicted the correct digit
+    if doodle_class == 1:
+        num_correct += 1
 
-    output = index_max(nn.feed_forward(normalize_v(airplane_data[i]).tolist()))
-    if output == 1:
-        numCorrect += 1
+    output = nn.compute(aircraft_carrier_data[i])
+    doodle_class = output.index(max(output))
 
-    output = index_max(nn.feed_forward(normalize_v(aircraft_carrier_data[i]).tolist()))
-    if output == 2:
-        numCorrect += 1
+    # If the NN predicted the correct digit
+    if doodle_class == 2:
+        num_correct += 1
 
-print(numCorrect / 6000)
+accuracy = num_correct / (training_size * 3)
+print("Training accuracy: " + str(accuracy))
+
+test_size = 1000
+
+num_correct = 0
+
+for i in range(0, test_size):
+    # The output will be a vector of probabilities. We use the largest value
+    # in this vector to determine the digit that the NN recognized.
+    output = nn.compute(car_data[i + 10000])
+    doodle_class = output.index(max(output))
+
+    # If the NN predicted the correct digit
+    if doodle_class == 0:
+        num_correct += 1
+
+    output = nn.compute(airplane_data[i + 10000])
+    doodle_class = output.index(max(output))
+
+    # If the NN predicted the correct digit
+    if doodle_class == 1:
+        num_correct += 1
+
+    output = nn.compute(aircraft_carrier_data[i + 10000])
+    doodle_class = output.index(max(output))
+
+    # If the NN predicted the correct digit
+    if doodle_class == 2:
+        num_correct += 1
+
+accuracy = num_correct / (test_size * 3)
+print("Test accuracy: " + str(accuracy))
+
+np.save("weights0", nn.weights[0])
+np.save("weights1", nn.weights[1])
+np.save("weights2", nn.weights[2])
+
+np.save("biases0", nn.biases[0])
+np.save("biases1", nn.biases[1])
+np.save("biases2", nn.biases[2])
--- neural_network.py
+++ neural_network.py
@@ -1,72 +1,92 @@
 import numpy as np
 import math
 
-def sigmoid(x):
-    if x < 0:
-        return 1 - 1 / (1 + math.exp(x))
-    return 1 / (1 + math.exp(-x))
+def sigmoid(gamma):
+  if gamma < 0:
+    return 1 - 1 / (1 + math.exp(gamma))
+  else:
+    return 1 / (1 + math.exp(-gamma))
+
+v_sigmoid = np.vectorize(sigmoid)
+
+def deriv_sigmoid(a):
+    return a * (1 - a)
+
+v_deriv_sigmoid = np.vectorize(deriv_sigmoid)
+
+# Cost function for individual neuron
+def cost_function(output, target):
+    # Add 0.0001 to avoid having to evaluate math.log(0)
+    return target * math.log(output + 0.0001) + (1 - target) * math.log(1 - output + 0.0001)
 
 class NeuralNetwork:
-    def __init__(self, numInputs, numHidden, numOutputs):
-        self.numInputs = numInputs
-        self.numHidden = numHidden
-        self.numOutputs = numOutputs
+    def __init__(self, layer_sizes, learning_rate):
+        self.learning_rate = learning_rate
+        self.layers = []
+        for size in layer_sizes:
+            self.layers.append(np.zeros(size))
 
-        self.weights_ih = np.random.uniform(low = -1.0, high = 1.0, size=(self.numHidden, self.numInputs))
-        self.weights_ho = np.random.uniform(low = -1.0, high = 1.0, size=(self.numOutputs, self.numHidden))
+        self.weights = []
+        self.biases = []
+        for i in range(0, len(self.layers) - 1):
+            # Initialises weights to random values between -1 and 1
+            random_weights = np.random.rand(self.layers[i + 1].size, self.layers[i].size)
+            random_weights = 2 * random_weights - 1
+            self.weights.append(random_weights)
 
-        self.biases_h = np.random.uniform(low = -1.0, high = 1.0, size=(self.numHidden, 1))
-        self.biases_o = np.random.uniform(low = -1.0, high = 1.0, size=(self.numOutputs, 1))
+            # Initialises biases to random values between -1 and 1
+            random_biases = np.random.rand(self.layers[i + 1].size)
+            random_biases = 2 * random_biases - 1
+            self.biases.append(random_biases)
 
-        self.learning_rate = 0.2
+    # Computes the output from the given input using the current weights and biases
+    def compute(self, input):
+        self.layers[0] = np.array(input)
+        for i in range(0, len(self.layers) - 1):
+            self.layers[i + 1] = np.matmul(self.weights[i], self.layers[i]) + self.biases[i]
+            self.layers[i + 1] = v_sigmoid(self.layers[i + 1])
+        # Returns the output layer as a list
+        return self.layers[-1].tolist()
 
-    def feed_forward(self, inputs):
-        sigmoid_v = np.vectorize(sigmoid)
-        inputs = np.array(inputs)
-        inputs = np.reshape(inputs, (self.numInputs, 1))
+    # Calculates the value of the cost function over the supplied data set
+    # Useful for diagnosing underfitting/overfitting and also to check for convergence
+    def calculate_cost(self, data, targets):
+        sum = 0
+        for i in range(0, len(data)):
+            output = self.compute(data[i])
+            for j in range(0, len(output)):
+                sum += cost_function(output[j], targets[i][j])
+        return sum / len(data) * -1
 
-        hidden = np.matmul(self.weights_ih, inputs) + self.biases_h
-        hidden = sigmoid_v(hidden)
+    def backprop(self, target):
+        # Each element of error corresponds to the errors for one layer
+        # Here 0 is simply used as a placeholder
+        error = [0] * (len(self.layers) - 1)
 
-        output = np.matmul(self.weights_ho, hidden) + self.biases_o
-        output = sigmoid_v(output)
+        # Calculate error for output layer
+        error[-1] = (target - self.layers[-1])
 
-        return output.tolist()
+        # Calculate error for hidden layers
+        for i in range(len(error) - 2, -1, -1):
+            transposed = np.transpose(self.weights[i + 1])
+            error[i] = np.matmul(transposed, error[i + 1]) * v_deriv_sigmoid(self.layers[i + 1])
 
-    def train(self, inputs, targets):
-        sigmoid_v = np.vectorize(sigmoid)
-        inputs = np.array(inputs)
-        inputs = np.reshape(inputs, (self.numInputs, 1))
+        # Calculate the changes that need to be made to the weights and biases
+        # in order to reduce the cost function value.
+        delta_weights = []
+        delta_biases = []
+        for i in range(0, len(self.weights)):
+            error_reshape = np.reshape(error[i], (error[i].size, 1))
+            layer_reshape = np.reshape(self.layers[i], (1, self.layers[i].size))
+            delta_weights.append(np.matmul(error_reshape, layer_reshape))
+            delta_biases.append(error[i])
 
-        hidden = np.matmul(self.weights_ih, inputs) + self.biases_h
-        hidden = sigmoid_v(hidden)
+        return delta_weights, delta_biases
 
-        output = np.matmul(self.weights_ho, hidden) + self.biases_o
-        output = sigmoid_v(output)
-
-        targets = np.array(targets)
-        targets = np.reshape(targets, (self.numOutputs, 1))
-
-        output_errors = targets - output
-
-        func = np.vectorize(lambda x : 1 - x)
-
-        # Calculate deltas for weights and biases between hidden and output layer
-        gradients_o = output * func(output)
-        gradients_o = gradients_o * output_errors
-        gradients_o = gradients_o * self.learning_rate
-
-        delta_weights_ho = np.matmul(gradients_o, np.transpose(hidden))
-        self.weights_ho += delta_weights_ho
-        self.biases_o += gradients_o
-
-        hidden_errors = np.matmul(np.transpose(self.weights_ho), output_errors)
-
-        # Calculate deltas for weights and biases between input and hidden layer
-        gradients_h = hidden * func(hidden)
-        gradients_h = gradients_h * hidden_errors
-        gradients_h = gradients_h * self.learning_rate
-
-        delta_weights_ih = np.matmul(gradients_h, np.transpose(inputs))
-        self.weights_ih += delta_weights_ih
-        self.biases_h += gradients_h
+    # Performs stochastic gradient descent on a single piece of data
+    def train(self, input, target):
+        self.compute(input)
+        dw, db = self.backprop(target)
+        for j in range(0, len(self.weights)):
+            self.weights[j] += self.learning_rate * dw[j]
+            self.biases[j] += self.learning_rate * db[j]
